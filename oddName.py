e164123fb3f64125c8dc47b1f645dc4920957c11
jc194412 <adam.davey1@my.jcu.edu.au>
2017-08-15 16:09:13 +1000

    "a meaningful commit message" :P week 4 prac 3 github add paper program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,11 @@
 """Adam Davey"""
+name = str(input("what is your name?:"))
+
+while len(name) == "":
+        print("invalid name")
+name = str(input("what is your name?:"))
+
+
+
+
+print(name[::2])
